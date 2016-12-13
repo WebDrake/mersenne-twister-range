e032e5c7fb2b1122de66d1a2fcc60372ebce0b9a
Joseph Rushton Wakeling <joe@webdrake.net>
2016-12-13 19:31:11 +0100

    Introduce basic benchmark with identical code to Mir
    Two local copies of the MT code are included, one control copy that will
    not change, one that will be converted to a range.

---
+++ src/app.d
@@ -0,0 +1,95 @@
+import opcallMT = mersenne_twister_opcall;
+import rangeMT = mersenne_twister_range;
+/+
+Mir 32-bit Mt19937: 6.80851 Gb/s
+check sum = 214724495853043511
+
+Mir 64-bit Mt19937: 12.5984 Gb/s
+check sum = 8857660070542815631
+
+Phobos 32-bit Mt19937: 4.41989 Gb/s
+check sum = 214724495853043511
+
+Phobos 64-bit Mt19937: wrong initialization and tempering
+
+1.54 = 6.80851 / 4.41989
+2.85 = 12.5984 / 4.41989
++/
+
+static import std.random;
+
+import std.traits;
+import std.stdio, std.datetime, std.conv;
+
+
+void main()
+{
+    auto seed = opcallMT.Mt19937_32.defaultSeed;
+    auto std = std.random.Mt19937(seed); //32-bit
+    auto mir_32o = opcallMT.Mt19937_32(seed);
+    auto mir_64o = opcallMT.Mt19937_64(seed);
+    auto mir_32r = rangeMT.Mt19937_32(seed);
+    auto mir_64r = rangeMT.Mt19937_64(seed);
+    enum ulong count = 100_000_000;
+    ulong s;
+    StopWatch sw;
+    sw.start;
+    foreach(_; 0..count) //boost CPU
+    {
+        s += std.front;
+        std.popFront;
+        s += mir_32o();
+        s += mir_64o();
+        s += mir_32r();
+        s += mir_64r();
+    }
+    s = 0;
+    sw.stop;
+    sw.reset;
+    sw.start;
+    foreach(_; 0..count)
+        s += mir_32o();
+    sw.stop;
+    writefln("opCall Mir 32-bit Mt19937: %s Gb/s", double(count * 32u) / sw.peek.msecs / 1000 ^^ 2);
+    writeln("check sum = ", s, "\n");
+    s = 0;
+    sw.reset;
+    sw.start;
+    foreach(_; 0..count)
+    {
+        s += mir_32r();
+    }
+    sw.stop;
+    writefln("range Mir 32-bit Mt19937: %s Gb/s", double(count * 32u) / sw.peek.msecs / 1000 ^^ 2);
+    writeln("check sum = ", s, "\n");
+    s = 0;
+    sw.reset;
+    sw.start;
+    foreach(_; 0..count)
+        s += mir_64o();
+    sw.stop;
+    writefln("opCall Mir 64-bit Mt19937: %s Gb/s", double(count * 64u) / sw.peek.msecs / 1000 ^^ 2);
+    writeln("check sum = ", s, "\n");
+    s = 0;
+    sw.reset;
+    sw.start;
+    foreach(_; 0..count)
+    {
+        s += mir_64r();
+    }
+    sw.stop;
+    writefln("range Mir 64-bit Mt19937: %s Gb/s", double(count * 64u) / sw.peek.msecs / 1000 ^^ 2);
+    writeln("check sum = ", s, "\n");
+    s = 0;
+    sw.reset;
+    sw.start;
+    foreach(_; 0..count)
+    {
+        s += std.front;
+        std.popFront();
+    }
+    sw.stop;
+    writefln("Phobos 32-bit Mt19937: %s Gb/s", double(count * 32u) / sw.peek.msecs / 1000 ^^ 2);
+    writeln("check sum = ", s, "\n");
+    writefln("Phobos 64-bit Mt19937: wrong initialization and tempering");
+}
---
+++ src/mersenne_twister_opcall.d
@@ -0,0 +1,199 @@
+/++
+The Mersenne Twister generator.
+
+Copyright: Copyright Andrei Alexandrescu 2008 - 2009, Ilya Yaroshenko 2016-.
+License:   $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+Authors: $(HTTP erdani.org, Andrei Alexandrescu) Ilya Yaroshenko (rework)
++/
+module mersenne_twister_opcall;
+
+// Segments of the code in this file Copyright (c) 1997 by Rick Booth
+// From "Inner Loops" by Rick Booth, Addison-Wesley
+
+// Work derived from:
+
+/*
+   A C-program for MT19937, with initialization improved 2002/1/26.
+   Coded by Takuji Nishimura and Makoto Matsumoto.
+
+   Before using, initialize the state by using init_genrand(seed)
+   or init_by_array(init_key, key_length).
+
+   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+     1. Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+     2. Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+     3. The names of its contributors may not be used to endorse or promote
+        products derived from this software without specific prior written
+        permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+   Any feedback is very welcome.
+   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+*/
+
+import std.traits;
+
+/++
+The $(LUCKY Mersenne Twister) generator.
++/
+struct MersenneTwisterEngine(Uint, size_t w, size_t n, size_t m, size_t r,
+                             Uint a,
+                             uint u, Uint d,
+                             uint s, Uint b,
+                             uint t, Uint c,
+                             uint l)
+    if (isUnsigned!Uint)
+{
+    static assert(0 < w && w <= Uint.sizeof * 8);
+    static assert(1 <= m && m <= n);
+    static assert(0 <= r && 0 <= u && 0 <= s && 0 <= t && 0 <= l);
+    static assert(r <= w && u <= w && s <= w && t <= w && l <= w);
+    static assert(0 <= a && 0 <= b && 0 <= c);
+
+    @disable this();
+    @disable this(this);
+
+    private enum Uint upperMask = ~((cast(Uint) 1u << (Uint.sizeof * 8 - (w - r))) - 1);
+    private enum Uint lowerMask = (cast(Uint) 1u << r) - 1;
+
+    /**
+    Parameters for the generator.
+    */
+    enum size_t wordSize   = w;
+    enum size_t stateSize  = n; /// ditto
+    enum size_t shiftSize  = m; /// ditto
+    enum size_t maskBits   = r; /// ditto
+    enum Uint xorMask    = a; /// ditto
+    enum uint temperingU = u; /// ditto
+    enum Uint temperingD = d; /// ditto
+    enum uint temperingS = s; /// ditto
+    enum Uint temperingB = b; /// ditto
+    enum uint temperingT = t; /// ditto
+    enum Uint temperingC = c; /// ditto
+    enum uint temperingL = l; /// ditto
+
+    /// Largest generated value.
+    enum Uint max = Uint.max >> (Uint.sizeof * 8u - w);
+    static assert(a <= max && b <= max && c <= max);
+
+    /// The default seed value.
+    enum Uint defaultSeed = 5489;
+
+    private Uint _z = void;
+    /++
+    Current reversed payload index with initial value equals to `n-1`
+    +/
+    private Uint index = void;
+    /++
+    Reversed(!) payload.
+    +/
+    Uint[n] data = void;
+
+    /**
+       Constructs a MersenneTwisterEngine object.
+    */
+    this(Uint value) @safe pure nothrow @nogc
+    {
+        static if (w == Uint.sizeof * 8)
+        {
+            data[$-1] = value;
+        }
+        else
+        {
+            static assert(max + 1 > 0);
+            data[$-1] = value % (max + 1);
+        }
+        static if (is(Uint == uint))
+            enum Uint f = 1812433253;
+        else
+        static if (is(Uint == ulong))
+            enum Uint f = 6364136223846793005;
+        else
+        static assert(0, "ucent is not supported by MersenneTwisterEngine.");
+        foreach_reverse (size_t i, ref e; data[0 .. $-1])
+            e = f * (data[i + 1] ^ (data[i + 1] >> (w - 2))) + cast(Uint)(n - (i + 1));
+        index = n-1;
+        opCall();
+    }
+
+    /++
+    Advances the generator.
+    +/
+    Uint opCall() @safe pure nothrow @nogc
+    {
+        sizediff_t index = cast(size_t)this.index;
+        sizediff_t next = index - 1;
+        if(next < 0)
+            next = n - 1;
+        auto z = _z;
+        sizediff_t conj = index - m;
+        if(conj < 0)
+            conj = index - m + n;
+        static if (d == Uint.max)
+            z ^= (z >> u);
+        else
+            z ^= (z >> u) & d;
+        auto q = data[index] & upperMask;
+        auto p = data[next] & lowerMask;
+        z ^= (z << s) & b;
+        auto y = q | p;
+        auto x = y >> 1;
+        z ^= (z << t) & c;
+        if (y & 1)
+            x ^= a;
+        auto e = data[conj] ^ x;
+        z ^= (z >> l);
+        _z = data[index] = e;
+        this.index = cast(Uint)next;
+        return z;
+    }
+}
+
+/++
+A $(D MersenneTwisterEngine) instantiated with the parameters of the
+original engine $(HTTP en.wikipedia.org/wiki/Mersenne_Twister,
+MT19937), generating uniformly-distributed 32-bit numbers with a
+period of 2 to the power of 19937.
++/
+alias Mt19937_32 = MersenneTwisterEngine!(uint, 32, 624, 397, 31,
+                                       0x9908b0df,
+                                       11, 0xffffffff,
+                                        7, 0x9d2c5680,
+                                       15, 0xefc60000,
+                                       18);
+/++
+A $(D MersenneTwisterEngine) instantiated with the parameters of the
+original engine $(HTTP en.wikipedia.org/wiki/Mersenne_Twister,
+MT19937), generating uniformly-distributed 64-bit numbers with a
+period of 2 to the power of 19937.
++/
+alias Mt19937_64 = MersenneTwisterEngine!(ulong, 64, 312, 156, 31,
+                                       0xb5026f5aa96619e9,
+                                       29, 0x5555555555555555,
+                                       17, 0x71d67fffeda60000,
+                                       37, 0xfff7eee000000000,
+                                       43);
